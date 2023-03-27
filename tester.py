3cbed5665e3b8d5a2282079df01c02237ca6da8c
Yi Yao <leaf_yao8621@hotmail.com>
2023-03-27 15:06:16 -0700

    Updates

--- tester.py
+++ tester.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     for i in range(10):
         for j in range(10):
             payload = {"a": i, "b": j}
+            print(payload)
             res = requests.post("http://localhost:8000/add", json=payload)
             print(res.json())
             res.close()
