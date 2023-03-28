be7362a8ddb2442f560cd2e673723736d4539942
Yi Yao <leaf_yao8621@hotmail.com>
2023-03-27 19:58:36 -0700

    Bug fix

--- src/server/router.c
+++ src/server/router.c
@@ -21,7 +21,7 @@ bool eq_url_method(URLMethod *a, URLMethod *b) {
     String *iter_a = a->url.breakdown.data;
     String *iter_b = b->url.breakdown.data;
     for (size_t i = 0; i < a->url.breakdown.size; ++i, ++iter_a, ++iter_b) {
-        if (iter_a->size == 1 && iter_a->data[0] == '*') {
+        if (iter_a->size == 2 && iter_a->data[0] == '*') {
             continue;
         }
         if (!containers_eq_dstr(iter_a, iter_b)) {
